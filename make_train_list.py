00d73db9382c4f67ce6fa2a4b0209a6ba2a7524e
zzzixun <71616472+zzzixun@users.noreply.github.com>
2025-07-12 16:00:14 +0800

    Update make_train_list.py

--- make_train_list.py
+++ make_train_list.py
@@ -1,15 +1,7 @@
 import os
-
-image_dir = 'VOC2012/JPEGImages'
-label_dir = 'VOC2012/SegmentationClassRaw'  # 建議先轉成灰階 index label
-list_file = 'train_list.txt'
-
-with open(list_file, 'w') as f:
-    for fname in os.listdir(image_dir):
-        if not fname.endswith('.jpg'):
-            continue
-        name = fname[:-4]
-        image_path = os.path.join(image_dir, name + '.jpg')
-        label_path = os.path.join(label_dir, name + '.png')
-        if os.path.exists(label_path):
-            f.write(f'{image_path} {label_path}\n')
+ids = open('/home/cbel/Desktop/zixun/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt').read().split()
+with open('train_list.txt','w') as f:
+    for id in ids:
+        img = os.path.join(IMG_DIR, id + '.jpg')
+        lbl = os.path.join(LABEL_DIR, id + '.png')
+        f.write(f"{img} {lbl}\n")
